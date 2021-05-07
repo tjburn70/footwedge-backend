7f8c4c99938c60bdee17f099c872d75f7cbe9753
Tristan Burnham <tjburn70@gmail.com>
2021-05-07 16:44:28 -0500

    Add user router

--- api/src/v1/router.py
+++ api/src/v1/router.py
@@ -3,10 +3,12 @@ from fastapi import APIRouter
 from .endpoints import (
     golf_club,
     golf_course,
-    health
+    health,
+    user
 )
 
 v1__router = APIRouter()
 v1__router.include_router(health.router)
 v1__router.include_router(golf_club.router, prefix=f"/{golf_club.PATH_PREFIX}")
 v1__router.include_router(golf_course.router, prefix=f"/{golf_course.PATH_PREFIX}")
+v1__router.include_router(user.router, prefix=f"/{user.PATH_PREFIX}")
