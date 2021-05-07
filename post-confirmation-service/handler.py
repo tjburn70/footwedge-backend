90865489610fa0460a37e59a5eadd611e7c5be15
Tristan Burnham <tjburn70@gmail.com>
2021-05-07 11:26:13 -0500

    cognito hook lambda

---
+++ post-confirmation-service/handler.py
@@ -0,0 +1,34 @@
+import json
+import os
+import logging
+
+import requests
+
+FOOTWEDGE_API_BASE_URL = os.getenv("FOOTWEDGE_API_BASE_URL")
+logger = logging.getLogger(__name__)
+logger.setLevel(level=logging.INFO)
+
+
+def post_user(user_body: dict):
+    url = f"{FOOTWEDGE_API_BASE_URL}/users"
+    resp = requests.post(
+        url=url,
+        json=user_body,
+    )
+    if not resp.ok:
+        logger.error(msg=f"Failed to create user, due to: {resp.text}")
+        return
+
+    user_email = user_body['email']
+    logger.info(f"Successfully created user with email: {user_email}")
+
+
+def create_user(event: dict) -> dict:
+    user_attributes = event["request"]["userAttributes"]
+    post_user(user_body=user_attributes)
+    return event
+
+
+def lambda_handler(event, context):
+    print(json.dumps(event))
+    return create_user(event)
