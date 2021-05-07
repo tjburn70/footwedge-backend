3a62aa02f91189e622e2c193ad68ee0877b074d0
Tristan Burnham <tjburn70@gmail.com>
2021-05-07 13:49:21 -0500

    Update user_body

--- post-confirmation-service/handler.py
+++ post-confirmation-service/handler.py
@@ -9,7 +9,16 @@ logger = logging.getLogger(__name__)
 logger.setLevel(level=logging.INFO)
 
 
-def post_user(user_body: dict):
+def post_user(username: str, user_attributes: dict):
+    user_body = {
+        "user_id": username,
+        "email": user_attributes["email"],
+        "first_name": user_attributes["custom:firstName"],
+        "last_name": user_attributes["custom:lastName"],
+        "birthdate": user_attributes["birthdate"],
+        "phone_number": user_attributes["phone_number"],
+        "gender": user_attributes["gender"],
+    }
     url = f"{FOOTWEDGE_API_URL}/users"
     resp = requests.post(
         url=url,
@@ -25,7 +34,8 @@ def post_user(user_body: dict):
 
 def create_user(event: dict) -> dict:
     user_attributes = event["request"]["userAttributes"]
-    post_user(user_body=user_attributes)
+    username = event["userName"]
+    post_user(username=username, user_attributes=user_attributes)
     return event
 
 
