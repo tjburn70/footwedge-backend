b6a41bff352f1ea5fb2d9af8e670f6ba8377706f
Tristan Burnham <tjburn70@gmail.com>
2021-05-14 17:41:48 -0500

    Add settings to stream-service

---
+++ stream-service/src/settings.py
@@ -0,0 +1,12 @@
+from pydantic import BaseSettings, AnyHttpUrl
+
+
+class Settings(BaseSettings):
+    COGNITO_DOMAIN: str = 'footwedge-dev'
+    COGNITO_REGION: str = 'us-east-2'
+    STREAM_SERVICE_COGNITO_CLIENT_ID: str
+    STREAM_SERVICE_COGNITO_CLIENT_SECRET: str
+    FOOTWEDGE_API_URL: AnyHttpUrl
+
+
+settings = Settings(_env_file='./src/.env', _env_file_encoding='utf-8')
