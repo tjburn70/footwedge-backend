1640fb90a87dd5adad9bf6da95cc2f05bb3a44c7
Tristan Burnham <tjburn70@gmail.com>
2021-05-16 11:05:39 -0500

    Add SEARCH_SERVICE_API_URL to settings

--- stream-service/src/settings.py
+++ stream-service/src/settings.py
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
     STREAM_SERVICE_COGNITO_CLIENT_ID: str
     STREAM_SERVICE_COGNITO_CLIENT_SECRET: str
     FOOTWEDGE_API_URL: AnyHttpUrl
+    SEARCH_SERVICE_API_URL: AnyHttpUrl
 
 
 settings = Settings(_env_file='./src/.env', _env_file_encoding='utf-8')
