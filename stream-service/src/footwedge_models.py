f01dad0eb7811a1af19313cb2f012c2d94eb021e
Tristan Burnham <tjburn70@gmail.com>
2021-05-16 11:05:07 -0500

    Add GolfCourse and GolfClub models

--- stream-service/src/footwedge_models.py
+++ stream-service/src/footwedge_models.py
@@ -1,6 +1,32 @@
-from datetime import date
+from datetime import date, datetime
 from decimal import Decimal
-from pydantic import BaseModel
+from typing import List, Optional
+
+from pydantic import AnyHttpUrl, BaseModel
+
+
+class GolfCourse(BaseModel):
+    golf_course_id: str
+    name: str
+    num_holes: int
+    created_ts: datetime
+    touched_ts: Optional[datetime] = None
+
+
+class GolfClub(BaseModel):
+    golf_club_id: str
+    name: str
+    address: Optional[str] = None
+    city: Optional[str] = None
+    state_code: Optional[str] = None
+    county: Optional[str] = None
+    zip_code: Optional[str] = None
+    phone_number: Optional[str] = None
+    email: Optional[str] = None
+    website: Optional[AnyHttpUrl] = None
+    created_ts: datetime
+    touched_ts: datetime = None
+    golf_courses: List[GolfCourse] = []
 
 
 class GolfRound(BaseModel):
@@ -22,3 +48,14 @@ class TeeBox(BaseModel):
     unit: str
     course_rating: Decimal
     slope: int
+
+
+class User(BaseModel):
+    user_id: str
+    email: str
+    first_name: str
+    last_name: str
+    full_name: str
+    birthdate: Optional[str] = None
+    phone_number: Optional[str] = None
+    gender: Optional[str] = None
