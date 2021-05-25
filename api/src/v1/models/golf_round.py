079a26d51f4f64f9abf52b43aa726ab927b527c3
Tristan Burnham <tjburn70@gmail.com>
2021-05-24 21:32:25 -0500

    Add round_type to GolfRoundBody

--- api/src/v1/models/golf_round.py
+++ api/src/v1/models/golf_round.py
@@ -1,4 +1,5 @@
 from datetime import datetime, date
+from enum import Enum
 from typing import (
     List,
     Optional,
@@ -9,12 +10,19 @@ from pydantic import BaseModel
 from .golf_round_stat import GolfRoundStat, GolfRoundStatBody
 
 
+class RoundType(str, Enum):
+    FRONT_NINE = "f9"
+    BACK_NINE = "b9"
+    EIGHTEEN_HOLES = "18"
+
+
 class GolfRoundBody(BaseModel):
     golf_course_id: str
     tee_box_id: str
     gross_score: int
     towards_handicap: bool = True
     played_on: date
+    round_type: Optional[RoundType] = RoundType.EIGHTEEN_HOLES
     stats: List[GolfRoundStatBody] = []
 
 
