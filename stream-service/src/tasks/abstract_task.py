121705d091d97e4c340b84c7a0b7e260215c1d8c
Tristan Burnham <tjburn70@gmail.com>
2021-05-28 17:07:25 -0500

    use get instead of key syntax

--- stream-service/src/tasks/abstract_task.py
+++ stream-service/src/tasks/abstract_task.py
@@ -9,7 +9,7 @@ class AbstractTask(metaclass=ABCMeta):
     def __init__(self, event_name: str, dynamodb_record: dict):
         self.event_name = event_name
         self.keys = dynamodb_record['Keys']
-        self.image = dynamodb_record['NewImage']
+        self.image = dynamodb_record.get('NewImage')
 
     @abstractmethod
     async def process_record(self):
