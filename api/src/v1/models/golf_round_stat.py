4cd319f4abcd84883ed67d675d0620f2d0c248e2
Tristan Burnham <tjburn70@gmail.com>
2021-05-28 07:34:39 -0500

    Add pydantic models

--- api/src/v1/models/golf_round_stat.py
+++ api/src/v1/models/golf_round_stat.py
@@ -19,3 +19,17 @@ class GolfRoundStat(GolfRoundStatBody):
     golf_round_stat_id: str
     created_ts: datetime
     touched_ts: Optional[datetime] = None
+
+
+class GolfRoundAggregateStats(BaseModel):
+    putts: int
+    fairways: int
+    greens_in_regulation: int
+    penalties: Optional[int] = 0
+    three_putts: Optional[int] = 0
+    up_and_downs: Optional[int] = 0
+    sand_saves: Optional[int] = 0
+    birdies: Optional[int] = 0
+    pars: Optional[int] = 0
+    bogeys: Optional[int] = 0
+    double_bogeys: Optional[int] = 0
