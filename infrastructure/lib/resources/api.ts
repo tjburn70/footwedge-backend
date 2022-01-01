5ac3af547bf3fa51a8e052201e24ec223833c5c0
Tristan Burnham <tjburn70@gmail.com>
2022-01-01 10:45:14 -0700

    add footwedgeApi infra

--- infrastructure/lib/resources/api.ts
+++ infrastructure/lib/resources/api.ts
@@ -2,6 +2,15 @@ import * as apigateway from '@aws-cdk/aws-apigateway'
 import * as cdk from '@aws-cdk/core'
 import * as lambda from '@aws-cdk/aws-lambda'
 
+export function generateFootwedgeApi(
+  scope: cdk.Construct,
+  footwedgeApiLambda: lambda.Function
+): apigateway.LambdaRestApi {
+  return new apigateway.LambdaRestApi(scope, 'FootwedgeApi', {
+    handler: footwedgeApiLambda,
+  })
+}
+
 export function generateSearchServiceApi(
   scope: cdk.Construct,
   searchServiceLambda: lambda.Function
--- infrastructure/lib/resources/lambda.ts
+++ infrastructure/lib/resources/lambda.ts
@@ -2,6 +2,42 @@ import * as cdk from '@aws-cdk/core'
 import * as lambda from '@aws-cdk/aws-lambda'
 import * as path from 'path'
 
+export interface FootwedgeApiProps {
+  envName: string,
+  serviceName: string,
+  cognitoRegion: string,
+  cognitoUserPoolId: string,
+  cognitoWebClientId: string,
+  dynamoDbUrl: string,
+  footwedgeDynamoTableName: string,
+}
+
+export function generateFootwedgeApiLambda(
+  scope: cdk.Construct,
+  props: FootwedgeApiProps
+): lambda.Function {
+  const id = 'footwedge-api'
+  return new lambda.Function(scope, id, {
+    runtime: lambda.Runtime.PYTHON_3_7,
+    code: lambda.Code.fromAsset(
+      path.join(__dirname, `../../../${id}/target`)
+    ),
+    handler: 'handler.lambda_handler',
+    functionName: `${props.envName}-${props.serviceName}-${id}`,
+    memorySize: 512,
+    timeout: cdk.Duration.minutes(5),
+    environment: {
+      ENV_NAME: props.envName,
+      PYTHONPATH: 'src',
+      COGNITO_REGION: props.cognitoRegion,
+      COGNITO_USER_POOL_ID: props.cognitoUserPoolId,
+      COGNITO_WEB_CLIENT_ID: props.cognitoWebClientId,
+      DYNAMO_DB_URL: props.dynamoDbUrl,
+      FOOTWEDGE_DYNAMO_TABLE: props.footwedgeDynamoTableName,
+    }
+  })
+}
+
 export function generateSearchServiceLambda(
   scope: cdk.Construct,
   envName: string,
